00e614b0c543b6c0fbadc29f3aee68c3e56fb1d0
Milad <milad.mosavi.developer@gmail.com>
2023-02-26 12:59:33 +0330

    add token manager

---
+++ googleSIgnIn/src/main/java/com/example/googlesignin/newSDK/isTokenExpired.kt
@@ -0,0 +1,19 @@
+package com.example.googlesignin.newSDK
+import org.json.JSONObject
+import android.util.Base64
+
+class TokenManger(){
+    fun isTokenExpired(token: String?): Boolean {
+        val parts = token?.split(".")
+        if (parts?.size != 3) {
+            return true
+        }
+        val payload = parts[1]
+        val payloadBytes = Base64.decode(payload, Base64.DEFAULT)
+        val jsonObject = JSONObject(String(payloadBytes))
+        val exp: Long = jsonObject.getLong("exp")
+        val currentTime = System.currentTimeMillis() / 1000
+        return currentTime > exp
+    }
+
+}
